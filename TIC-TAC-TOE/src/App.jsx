86c1d9bb40769b0acafed652379e617dc7c742b3
gabrielgadelia <166238189+gabrielgadelia@users.noreply.github.com>
2025-12-02 22:23:59 +0400

    isActive

--- TIC-TAC-TOE/src/App.jsx
+++ TIC-TAC-TOE/src/App.jsx
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import Player from "./components/Players";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+
+  function handleCurrentPlayerChange() {
+    setCurrentPlayer((prevValue) => (prevValue === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player name="Player 1" symbol="X" />
-          <Player name="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            currentPlayer={currentPlayer}
+            initialName="Player 1"
+            symbol="X"
+            isActive={currentPlayer === "X"}
+          />
+          <Player
+            currentPlayer={currentPlayer}
+            initialName="Player 2"
+            symbol="O"
+            isActive={currentPlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          currentPlayer={currentPlayer}
+          onSquareSeelct={handleCurrentPlayerChange}
+        />
       </div>
     </main>
   );
