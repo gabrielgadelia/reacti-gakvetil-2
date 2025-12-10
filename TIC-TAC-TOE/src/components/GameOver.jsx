4e9f04b96156e32531e46b41e2154124bff31d03
gabrielgadelia <166238189+gabrielgadelia@users.noreply.github.com>
2025-12-10 18:46:59 +0400

    avoie

--- TIC-TAC-TOE/src/components/GameOver.jsx
+++ TIC-TAC-TOE/src/components/GameOver.jsx
@@ -1,15 +1,3 @@
-// export default function GameOver() {
-//   return (
-//     <div id="game-over">
-//       <h2>Game Over</h2>
-//       <p>X won!</p>
-//       <p>
-//         <button>Rematch!</button>
-//       </p>
-//     </div>
-//   );
-// }
-
 export default function GameOver({ winner, onRestart }) {
   return (
     <div id="game-over">
